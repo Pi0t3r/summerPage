6af28fa28a2551eb2ed20dc69394e6852683609e
Pi0t3r <kapuscinskipiotr5@gmail.com>
2022-06-13 17:57:11 +0200

    fourth commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 const nav = document.querySelector('.nav')
 const btn = document.querySelector('.fa-bars')
 const allNavItems = document.querySelectorAll('.effect')
+const close = document.querySelector('.fa-circle-xmark')
+
 
 const handleNav = () => {
     nav.classList.toggle('active')
-
     allNavItems.forEach(item => {
         item.addEventListener('click', () =>{
             nav.classList.remove('active')
         })
     })
+    
 }
-
 btn.addEventListener('click',handleNav)
